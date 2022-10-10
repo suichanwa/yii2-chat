03b74c8e8d8d0aea4010c57a743c86ee8635fc40
suichanwa <augustinnegura746@gmail.com>
2022-10-10 09:40:19 +0300

    creating chat and controller

--- config/web.php
+++ config/web.php
@@ -42,14 +42,12 @@ $config = [
             ],
         ],
         'db' => $db,
-        /*
         'urlManager' => [
             'enablePrettyUrl' => true,
             'showScriptName' => false,
             'rules' => [
             ],
         ],
-        */
     ],
     'params' => $params,
 ];
@@ -64,10 +62,10 @@ if (YII_ENV_DEV) {
     ];
 
     $config['bootstrap'][] = 'gii';
+
     $config['modules']['gii'] = [
         'class' => 'yii\gii\Module',
-        // uncomment the following to add your IP if you are not connecting from localhost.
-        //'allowedIPs' => ['127.0.0.1', '::1'],
+        'allowedIPs' => ['127.0.0.1', '::1'],
     ];
 }
 
